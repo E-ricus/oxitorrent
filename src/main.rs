a047fd4e5b95a2315d2a4799397687db88887015
Eric Puentes <eric.david2333@gmail.com>
2023-10-24 18:18:12 +0200

    stage 6

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, Subcommand};
-use serde::Deserialize;
+use pieces::Pieces;
+use serde::{Deserialize, Serialize};
 use serde_json::{self, Value};
+use sha1::{Digest, Sha1};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
@@ -22,14 +24,13 @@ struct Torrent {
     info: Info,
 }
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 struct Info {
     length: usize,
-    // Unused by now
-    // name: String,
-    // #[serde(rename = "piece length")]
-    // plength: usize,
-    // pieces: Vec<u8>,
+    name: String,
+    #[serde(rename = "piece length")]
+    plength: usize,
+    pieces: Pieces,
 }
 
 fn decode_bencoded_value(encoded_value: &str) -> (Value, &str) {
@@ -132,9 +133,70 @@ fn main() -> anyhow::Result<()> {
         Commands::Info { torrent } => {
             let file = std::fs::read(torrent)?;
             let t: Torrent = serde_bencode::from_bytes(&file)?;
+            let mut hasher = Sha1::new();
+            let encoded = serde_bencode::to_bytes(&t.info)?;
+            hasher.update(&encoded);
+            let info_hash = hasher.finalize();
+
             println!("Tracker URL: {}", t.announce);
             println!("Length: {}", t.info.length);
+            println!("Info Hash: {}", hex::encode(info_hash));
         }
     }
     Ok(())
+}
+
+mod pieces {
+    use std::fmt;
+
+    use serde::{
+        de::{self, Visitor},
+        Deserialize, Deserializer, Serialize, Serializer,
+    };
+
+    #[derive(Debug, Clone)]
+    pub struct Pieces(Vec<[u8; 20]>);
+
+    struct PiecesVisitor;
+
+    impl<'de> Visitor<'de> for PiecesVisitor {
+        type Value = Pieces;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a byte string with length multiple of 20")
+        }
+
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            if v.len() % 20 != 0 {
+                return Err(E::custom("length is not correct"));
+            }
+            Ok(Pieces(
+                v.chunks_exact(20)
+                    .map(|s| s.try_into().expect("length is 20"))
+                    .collect(),
+            ))
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Pieces {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_i32(PiecesVisitor)
+        }
+    }
+
+    impl Serialize for Pieces {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            let slice = self.0.concat();
+            serializer.serialize_bytes(&slice)
+        }
+    }
 }
