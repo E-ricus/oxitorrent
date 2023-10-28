5794868a3d1c80ff6e9ffa26bc628f6e96148270
Eric Puentes <eric.david2333@gmail.com>
2023-10-28 13:08:15 +0200

    encode and decoder

--- src/peer.rs
+++ src/peer.rs
@@ -1,3 +1,6 @@
+use bytes::{Buf, BufMut, BytesMut};
+use tokio_util::codec::{Decoder, Encoder};
+
 #[repr(C)]
 pub struct Handshake {
     pub length: u8,
@@ -6,6 +9,7 @@ pub struct Handshake {
     pub info_hash: [u8; 20],
     pub peer_id: [u8; 20],
 }
+
 impl Handshake {
     pub fn new(info_hash: [u8; 20], peer_id: [u8; 20]) -> Self {
         Self {
@@ -22,4 +26,118 @@ pub fn as_bytes_mut(data: &mut Handshake) -> &mut [u8] {
     let ptr = data as *mut Handshake as *mut u8;
     let len = std::mem::size_of::<Handshake>();
     unsafe { std::slice::from_raw_parts_mut(ptr, len) }
+}
+
+pub struct Message {
+    tag: MessageTag,
+    payload: Vec<u8>,
+}
+
+#[repr(u8)]
+pub enum MessageTag {
+    Choke = 0,
+    Unchoke = 1,
+    Interested = 2,
+    NotInterested = 3,
+    Have = 4,
+    Bitfield = 5,
+    Request = 6,
+    Piece = 7,
+    Cancel = 8,
+}
+
+struct MessageFramer;
+
+const MAX: usize = 1 << 16;
+
+impl Decoder for MessageFramer {
+    type Item = Message;
+    type Error = std::io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if src.len() < 5 {
+            // Not enough data to read length marker.
+            return Ok(None);
+        }
+
+        // Read length marker.
+        let mut length_bytes = [0u8; 4];
+        length_bytes.copy_from_slice(&src[..4]);
+        let length = u32::from_be_bytes(length_bytes) as usize;
+
+        // Check that the length is not too large to avoid a denial of
+        // service attack where the server runs out of memory.
+        if length > MAX {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame of length {} is too large.", length),
+            ));
+        }
+
+        if src.len() < 4 + length {
+            // The full message has not yet arrived.
+            //
+            // We reserve more space in the buffer. This is not strictly
+            // necessary, but is a good idea performance-wise.
+            src.reserve(4 + length - src.len());
+
+            // We inform the Framed that we need more bytes to form the next
+            // frame.
+            return Ok(None);
+        }
+
+        // Use advance to modify src such that it no longer contains
+        // this frame.
+        let tag = src[4];
+        let tag = match tag {
+            0 => MessageTag::Choke,
+            1 => MessageTag::Unchoke,
+            2 => MessageTag::Interested,
+            3 => MessageTag::NotInterested,
+            4 => MessageTag::Have,
+            5 => MessageTag::Bitfield,
+            6 => MessageTag::Request,
+            7 => MessageTag::Piece,
+            8 => MessageTag::Cancel,
+            tag => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!("Unknown tag: {}", tag),
+                ));
+            }
+        };
+        let data = src[5..3 + length].to_vec();
+
+        src.advance(4 + length);
+
+        Ok(Some(Message { tag, payload: data }))
+    }
+}
+
+impl Encoder<Message> for MessageFramer {
+    type Error = std::io::Error;
+
+    fn encode(&mut self, item: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        // Don't send a string if it is longer than the other end will
+        // accept.
+        if item.payload.len() + 1 > MAX {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame of length {} is too large.", item.payload.len()),
+            ));
+        }
+
+        // Convert the length into a byte array.
+        // The cast to u32 cannot overflow due to the length check above.
+        let len_slice = u32::to_be_bytes(item.payload.len() as u32 + 1);
+
+        // Reserve space in the buffer.
+        dst.reserve(4 + item.payload.len() + 1);
+
+        // Write the length and string to the buffer.
+        dst.extend_from_slice(&len_slice);
+        dst.put_u8(item.tag as u8);
+        dst.extend_from_slice(&item.payload);
+        Ok(())
+    }
 }
